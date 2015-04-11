76219603431595095689680c76ef0677b401d650
Daren Cheng <2012DCheng@gmail.com>
2015-04-11 02:07:07 -0500

    Testing Commit with SourceControlTestClass.java under src/default

---
+++ claimsProcessingStudent/src/SourceControlTestClass.java
@@ -0,0 +1,13 @@
+
+public class SourceControlTestClass {
+	public SourceControlTestClass() {
+		int x = 7;
+		int y = 5;
+		float p = 0;
+		float q = 0;
+				
+		p = x + y;
+		q = x - p;
+		p = q++;
+	}
+}
