453f89cd8d6b9d8fa8503cd8246e7c1cacc64d3f
Cheng <Cheng@97.77.48.208>
2015-04-13 20:40:44 -0500

    foo test

--- claimsProcessingStudent/src/SourceControlTestClass.java
+++ claimsProcessingStudent/src/SourceControlTestClass.java
@@ -11,11 +11,7 @@ public class SourceControlTestClass {
 		p = x + 10;
 		
 		//conflict 2
-<<<<<<< HEAD
-		q = x - 4000;
-=======
-		q = x - 42;
->>>>>>> branch 'master' of https://github.com/redfeatherplusplus/4352-ActiveMQ-Project
+		p = p + 1;
 		
 		//test adding a line of code 
 		q++;
